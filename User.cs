33160d551eb4a65388ddc134119fac2f6b5361b5
Nurs Sargash <Nurs.Sargash@vu.com>
2021-01-12 23:41:35 -0600

    Add project files.

---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGit
+{
+    class User
+    {
+
+        private string name;
+        private int age;
+        private char gender;
+        private string adress;
+    }
+}
