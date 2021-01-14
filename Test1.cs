5d67798645822a04ae452b06d754c445eaff8772
snurs02 <nurs.sargash02@gmail.com>
2021-01-14 16:52:59 -0600

    Creates  a Test Class.

---
+++ Test1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGit
+{
+    class Test1
+    {
+
+
+        public void test1()
+        {
+
+        }
+
+        public void test2()
+        {
+
+        }
+    }
+}
