6b41128be3e74e8a2998a0616427940e53493dd0
snurs02 <nurs.sargash02@gmail.com>
2021-01-14 11:28:20 -0600

    Added a new file named AnyClass

---
+++ AnyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGit
+{
+    class AnyClass
+    {
+
+
+        public static void main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
